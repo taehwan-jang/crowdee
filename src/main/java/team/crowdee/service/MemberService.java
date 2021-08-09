2b24df3967a158db3a2101b8e56ecef0787f82c4
dahyun960724 <bae960724@naver.com>
2021-08-09 21:08:22 +0900

    서비스

--- src/main/java/team/crowdee/service/MemberService.java
+++ src/main/java/team/crowdee/service/MemberService.java
@@ -1,9 +1,21 @@
 package team.crowdee.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import team.crowdee.domain.Member;
+import team.crowdee.repository.MemberRepository;
 
 @Service
+@RequiredArgsConstructor
 public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public long join(Member member) {
+
+        memberRepository.save(member);
+        return member.getMemberId();
+    }
 
 
 
