85c05d1b1368e4194b652b58e2d9331723c09c9d
dohyeon1996 <sdh30113@naver.com>
2021-08-09 21:04:54 +0900

    성두현

--- src/main/java/team/crowdee/service/MemberService.java
+++ src/main/java/team/crowdee/service/MemberService.java
@@ -1,9 +1,26 @@
 package team.crowdee.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import team.crowdee.domain.Member;
+import team.crowdee.repository.MemberRepository;
 
 @Service
+@RequiredArgsConstructor
 public class MemberService {
+
+    private MemberRepository memberRepository;
+
+    private Member join(Member member) {
+        //this.validate(member);
+        memberRepository.save(member);
+        return member;
+
+    }
+
+  /*  private Boolean validate(Member member) {
+
+    }*/
 
 
 
---
+++ src/test/java/team/crowdee/repository/MemberRepositoryTest.java
@@ -0,0 +1,16 @@
+package team.crowdee.repository;
+
+import org.junit.jupiter.api.Test;
+import team.crowdee.domain.Member;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberRepositoryTest {
+
+    private MemberRepository memberRepository;
+
+
+
+
+
+}
