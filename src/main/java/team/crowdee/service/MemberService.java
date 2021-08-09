f3c91e558c59a200a9d48f1cfff863039bb4b578
dohyeon1996 <sdh30113@naver.com>
2021-08-09 21:15:04 +0900

    회원가입 아이디,패스워드유혀성검증

--- src/main/java/team/crowdee/service/MemberService.java
+++ src/main/java/team/crowdee/service/MemberService.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Service;
 import team.crowdee.domain.Member;
 import team.crowdee.repository.MemberRepository;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @Service
 @RequiredArgsConstructor
 public class MemberService {
@@ -12,16 +15,26 @@ public class MemberService {
     private MemberRepository memberRepository;
 
     private Member join(Member member) {
-        //this.validate(member);
+        this.validation(member);
         memberRepository.save(member);
         return member;
 
     }
 
-  /*  private Boolean validate(Member member) {
-
-    }*/
-
-
+   private  boolean validation(Member member){
+       Pattern p = Pattern.compile("\"^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]{8,}$\"\n");
+       Matcher m = p.matcher(member.getPassword());
+       if(member.getUserId().length()<4 || member.getUserId().length()>20) {
+           return false;
+       }
+       if(m.matches()){
+           return true;
+       }
+       return false;
+   }
 
 }
+
+
+
+
