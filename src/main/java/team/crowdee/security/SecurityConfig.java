08634e63603b1a88637fb5d36241c3482b7bc238
jangtaehwan <jth0602@gmail.com>
2021-08-13 16:50:09 +0900

    security 설정중

--- src/main/java/team/crowdee/security/SecurityConfig.java
+++ src/main/java/team/crowdee/security/SecurityConfig.java
@@ -28,20 +28,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic().disable()
-                .csrf()
-                .disable()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .authorizeRequests()
-                .antMatchers("/*")
-                .permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .cors()
-                .and();
-
+        http.
+                httpBasic().disable()
+                .cors().disable()
+                .csrf().disable();
 
     }
     @Bean
