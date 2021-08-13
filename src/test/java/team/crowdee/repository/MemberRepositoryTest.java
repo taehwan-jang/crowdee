6e378f9b8dcdc634406c3cd788b9785636488413
jangtaehwan <jth0602@gmail.com>
2021-08-13 13:09:25 +0900

    Repository 테스트 추가

---
+++ src/test/java/team/crowdee/repository/MemberRepositoryTest.java
@@ -0,0 +1,25 @@
+package team.crowdee.repository;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@Transactional
+class MemberRepositoryTest {
+
+
+    @Test
+    public void 테스트_() throws Exception {
+        //given
+
+        //when
+
+        //then
+    }
+}
