b06600564cff4579f92632b9ddefea1f4550b82c
jangtaehwan <jth0602@gmail.com>
2021-08-15 20:12:56 +0900

    JWT 로그인 controller 토큰발행 추가 / eamil 이슈 처리

--- src/main/java/team/crowdee/service/CustomUserDetailsService.java
+++ src/main/java/team/crowdee/service/CustomUserDetailsService.java
@@ -1,5 +1,6 @@
 package team.crowdee.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,6 +14,7 @@ import team.crowdee.repository.MemberRepository;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Component("userDetailsService")
 public class CustomUserDetailsService implements UserDetailsService {
 
@@ -34,6 +36,7 @@ public class CustomUserDetailsService implements UserDetailsService {
     // DB 에서 조회한 member 및 권한정보를 'org.springframework.security.core.userdetails.User' 객체로 변환하여 리턴
     private org.springframework.security.core.userdetails.User createUser(String userId, Member member){
         if(member==null){ // 수정필요한 코드
+            log.info("//==========로그인 실패시 로직===========//");
             throw new RuntimeException(userId + " -> 활성화되어 있지 않습니다.");
         }
         List<GrantedAuthority> grantedAuthorities = member.getAuthorities().stream()
