d6e44317f9f695bd2f05a745ee005216f24a425d
dohyeon1996 <sdh30113@naver.com>
2021-08-24 14:54:17 +0900

    컨트롤러,서비스,테스트 부분변경 회원가입,로그인,비번번호찾기,비밀번호변경,패스워드검증,회원정보수정,이메일인증,회원탈퇴

--- src/main/java/team/crowdee/controller/AdminController.java
+++ src/main/java/team/crowdee/controller/AdminController.java
@@ -1,13 +1,37 @@
 package team.crowdee.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import team.crowdee.domain.Member;
+import team.crowdee.repository.MemberRepository;
+import team.crowdee.service.MemberService;
+
+import javax.mail.MessagingException;
+import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/admin")
 public class AdminController {
-
     /**
      * admin 관련 로직
      */
+    private final MemberService memberService;
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/findMemberAll")
+    public ResponseEntity<?> allData() throws MessagingException {
+        List<Member> members = memberRepository.findAll();
+        if (members.isEmpty() && members!=null) {
+            return new ResponseEntity<>("회원조회가 완료되었습니다.", HttpStatus.OK);
+        }
+        else {
+            return new ResponseEntity<>("회원조회실패", HttpStatus.BAD_REQUEST);
+        }
+
+    }
 }
--- src/main/java/team/crowdee/security/SecurityConfig.java
+++ src/main/java/team/crowdee/security/SecurityConfig.java
@@ -17,7 +17,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
-import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
